4b04daf1abe99396c0291517483a9a3ce1fbb0ce
kusainovv <kusainovratmir89@gmail.com>
2025-06-03 16:03:33 +0400

    Refactor ESLint configuration and update dependencies
    - Standardized import statements to use single quotes across the ESLint configuration file.
    - Updated the lint command in package.json to remove `pnpm dlx` for direct execution.
    - Upgraded Husky dependency from version 9.1.7 to 8.0.0 in package.json and pnpm-lock.yaml.
    - Adjusted lint-staged configuration to run ESLint without the `--fix` option and changed Prettier to use `--check`.
    - Enhanced README.md to clarify code quality tools and usage instructions.
    - Cleaned up Vite configuration for better readability and consistency.
    - Minor adjustments to CSS imports for uniformity.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   js.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: {
       globals: globals.browser,
     },
@@ -15,10 +15,10 @@ export default defineConfig([
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{js,ts,jsx,tsx}"],
+    files: ['**/*.{js,ts,jsx,tsx}'],
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function App() {
-  const q = [];
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World 🌎</h1>

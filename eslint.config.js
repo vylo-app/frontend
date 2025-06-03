8f8fa3231a62a3a7a2237c23e9612ea0d41da6f6
kusainovv <kusainovratmir89@gmail.com>
2025-06-03 13:34:51 +0400

    Refactor ESLint configuration and update dependencies
    - Updated ESLint configuration to use `defineConfig` and include React plugin.
    - Expanded file patterns for linting to cover additional file types.
    - Removed deprecated plugins and added `eslint-plugin-import` and `eslint-plugin-jsx-a11y`.
    - Updated package dependencies to the latest versions, including ESLint and TypeScript-related packages.
    - Adjusted lint script in package.json to use `pnpm dlx` for better execution context.
    - Added React imports in App and main components for proper JSX handling.

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
+  js.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // TODO: Add rules only here @kusainovv
     },
   },
-)
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 
 function App() {
   return <h1 className="text-3xl font-bold underline">Hello World 🌎</h1>

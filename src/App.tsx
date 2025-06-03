6d71f93d934c5a9a80d0263cebaa272e367d18d6
kusainovv <kusainovratmir89@gmail.com>
2025-06-03 13:37:42 +0400

    Add Prettier configuration and update package dependencies
    - Introduced .prettierrc for code formatting with specified rules.
    - Added .prettierignore to exclude node_modules, dist, and build directories.
    - Updated package.json to include Prettier as a dependency and added a format script.
    - Adjusted imports in various files to use single quotes and added semicolons for consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React from 'react';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World 🌎</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World 🌎</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
